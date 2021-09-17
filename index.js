b16846986944438e9eea84c723a695540eec8be8
mi2507 <mreginadasilva@yahoo.com.br>
2021-09-17 15:35:13 -0300

    projeto aula 4 imersão dev

---
+++ index.js
@@ -0,0 +1,44 @@
+const filmes =
+    [
+        "https://br.web.img2.acsta.net/medias/nmedia/18/87/34/62/19874544.jpg",
+
+        "https://upload.wikimedia.org/wikipedia/pt/thumb/5/59/Captain_Marvel_%282018%29.jpg/250px-Captain_Marvel_%282018%29.jpg",
+        "https://m.media-amazon.com/images/I/81vTHovrz+L._AC_SL1500_.jpg",
+        "https://upload.wikimedia.org/wikipedia/pt/thumb/1/1b/The_Incredible_Hulk.jpg/250px-The_Incredible_Hulk.jpg",
+        "http://vortexcultural.com.br/images/2019/04/homem-de-ferro-2.jpg",
+        "https://upload.wikimedia.org/wikipedia/pt/thumb/9/9f/Thor_Poster.jpg/250px-Thor_Poster.jpg",
+        "https://img.elo7.com.br/product/original/2678478/big-poster-filme-os-vingadores-2012-lo02-tamanho-90x60-cm-vingadores-ultimato.jpg",
+        "https://upload.wikimedia.org/wikipedia/pt/1/19/Iron_Man_3_poster.jpg",
+        "https://upload.wikimedia.org/wikipedia/pt/b/b3/Thor_The_Dark_World_2013.jpg",
+        "https://upload.wikimedia.org/wikipedia/pt/e/e8/Captain_America_The_Winter_Soldier.jpg",
+        "https://a-static.mlcdn.com.br/1500x1500/dvd-vingadores-era-de-ultron-marvel/milesimo/539743149/a1a1f318684c6d01e636b1abb1357d00.jpg",
+        "https://br.web.img3.acsta.net/pictures/14/06/03/21/11/122582.jpg",
+        "https://br.web.img3.acsta.net/pictures/17/03/03/19/15/268212.jpg",
+        "https://upload.wikimedia.org/wikipedia/pt/thumb/9/90/Ant_Man-Poster.jpg/250px-Ant_Man-Poster.jpg",
+        "https://br.web.img3.acsta.net/c_310_420/pictures/16/03/10/20/36/363874.jpg",
+        "https://br.web.img2.acsta.net/pictures/17/12/07/16/09/2291532.jpg",
+        "https://br.web.img3.acsta.net/pictures/17/05/29/23/31/530814.jpg",
+        "https://upload.wikimedia.org/wikipedia/pt/thumb/c/c7/Doctor_Strange_poster.jpg/250px-Doctor_Strange_poster.jpg",
+        "https://images.moviefit.me/t/o/378-thor-ragnarok.jpg",
+        "https://www.ocamundongo.com.br/wp-content/uploads/2018/07/homem-formiga-vespa-resumo-poster-camundongo.jpg",
+        "https://lumiere-a.akamaihd.net/v1/images/image_a2514f6d.jpeg?region=0,0,540,810",
+        "https://br.web.img2.acsta.net/pictures/18/03/16/15/08/2019826.jpg",
+        "https://br.web.img3.acsta.net/pictures/19/04/26/17/30/2428965.jpg",
+        "https://br.web.img2.acsta.net/pictures/21/05/12/17/18/2995518.jpg",
+        "https://upload.wikimedia.org/wikipedia/pt/thumb/c/c9/Eternals_poster.jpg/250px-Eternals_poster.jpg",
+        "https://br.web.img2.acsta.net/pictures/19/07/05/17/30/5167951.jpg",
+        "https://terrigen-cdn-dev.marvel.com/content/prod/2x/wandavision_lob_crd_06.jpg",
+        "https://observatoriodocinema.uol.com.br/wp-content/uploads/2020/06/Doctor-Strange-2-Wanda-Fan-poster.jpg",
+        "https://disneyplusbrasil.com.br/wp-content/uploads/2021/04/Poster-Shang-Chi-1-819x1024.jpg",
+        "https://media.fstatic.com/cbkTwgUCqD6S1Urr1rwmyntsQEw=/290x478/smart/media/movies/covers/2021/03/159181327_708610536473577_8622802426976992635_o.jpg",
+        "http://www.cinetcetera.com.br/wp-content/uploads/2020/11/126322274_2791569164503185_2795416317938226743_n-819x1024.jpg",
+        "https://legadodamarvel.com.br/wp-content/uploads/2020/09/Fa-Poster-Pantera-Negra-2-Shuri-legadodamarvel.jpg",
+        "https://trecobox.com.br/wp-content/uploads/2020/04/capita-marvel-secret-invasion-poster-fanmade.jpg",
+    ];
+
+
+for (let i = 0; i < filmes.length; i++) {
+    document.write("<img src=" + filmes[i] + ">")
+}
+
+
